1291fe2af8e1fb1483a5be0d2870947a38ba3d28
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2024-09-02 11:31:21 -0500

    update git ignore

--- public/src/schemas/comment.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-const commentSchema = (0, zod_1.object)({
-    text: (0, zod_1.string)({ required_error: "text is required" })
-        .min(1, "Comment must be at least 1 character")
-});
-exports.default = commentSchema;
--- public/src/schemas/reaction.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-const reactionSchema = (0, zod_1.object)({
-    reaction: (0, zod_1.number)({ required_error: "number is required" })
-        .min(1, "the reactions go from 1 to 5")
-        .max(5, "the reactions go from 1 to 5")
-});
-exports.default = reactionSchema;
