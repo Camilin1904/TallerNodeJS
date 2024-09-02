cbc076e7e49dee0c5f356a4b0dadac5810bf26b4
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2024-09-02 10:33:22 -0500

    schemas

--- src/schemas/comment.schema.ts
+++ src/schemas/comment.schema.ts
@@ -2,6 +2,7 @@ import {object, string} from "zod";
 
 const commentSchema = object({
     text:string({required_error:"text is required"})
+    .min(1, "Comment must be at least 1 character")
 })
 
 export default commentSchema;
--- src/schemas/reaction.schema.ts
+++ src/schemas/reaction.schema.ts
@@ -1,7 +1,9 @@
 import {number, object, string} from "zod";
 
 const reactionSchema = object({
-    reaction:number({required_error:"number is required, the reactions go from 1 to 5"})
+    reaction:number({required_error:"number is required"})
+        .min(1, "the reactions go from 1 to 5")
+        .max(5, "the reactions go from 1 to 5")
 })
 
 export default reactionSchema;
